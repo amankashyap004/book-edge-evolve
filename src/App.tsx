ea6669e0a414cc3d8baf362c02a21d4e6a1585ff
amankashyap004 <amankashyap004@gmail.com>
2024-08-14 13:41:16 +0530

    feat: folder and file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomePage from "./components/home/HomePage";
 
 function App() {
-  return <div>Book Edge Evolve</div>;
+  return (
+    <main>
+      <Header />
+      <HomePage />
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
