9b2ec22ad9e43c27c8d246f5eb3bba55352e1c1b
amankashyap004 <amankashyap004@gmail.com>
2024-08-15 21:11:01 +0530

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
@@ -22,7 +22,7 @@ function App() {
     };
     aosAnimation();
   }, []);
-  
+
   return (
     <main
       className={
